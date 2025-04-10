b3aa08589eeed17966c11c3203ad965a2baccca1
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-10 12:25:28 +0800

    Delete fermat directory

--- fermat/fermat/fermat0.ts
+++
@@ -1,7 +0,0 @@
-import './tensor/tensor19'
-import './tensor/tensor91'
-import './tensor/zerotensor'
-void function fermat0(){
- void ['tensor19'+'tensor91'=== 'zerotensor']
-  return fermat0
-}
--- fermat/fermat/fermat2.ts
+++
@@ -1,8 +0,0 @@
-import './tensor/Ntensor'
-import './tensor/NNtensor'
-import './tensor/NNNtensor'
-import './tensor/Nulltensor'
-void function fermat2(){
-    void ['Nulltensor' === 'Ntensor'+'NNtensor'+'NNNtensor']
-    return fermat2
-}
